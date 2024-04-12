5b863b58e54e7032ceaafac78b2077b68b8d5945
Karidus-423 <kapuerto23@gmail.com>
2024-04-12 14:46:59 -0400

    resume update #1

--- components/navbar.js
+++ components/navbar.js
@@ -3,18 +3,18 @@ import Logo from './logo'
 import NextLink from 'next/link'
 import { CircleOption } from './icons/circle'
 import {
-    Container,
-    Box,
-    Link,
-    Stack,
-    Heading,
-    Flex,
-    Menu,
-    MenuItem,
-    MenuList,
-    MenuButton,
-    IconButton,
-    useColorModeValue
+	Container,
+	Box,
+	Link,
+	Stack,
+	Heading,
+	Flex,
+	Menu,
+	MenuItem,
+	MenuList,
+	MenuButton,
+	IconButton,
+	useColorModeValue
 } from '@chakra-ui/react'
 import { IoLogoGithub } from 'react-icons/io5'
 import { HamburgerIcon } from '@chakra-ui/icons'
@@ -22,117 +22,117 @@ import ThemeToggleButton from './theme-toggle-button'
 import { useRouter } from 'next/router'
 
 const LinkItem = ({ href, target, children, ...props }) => {
-    const router = useRouter();
-    const active = router.pathname === href
-    const inactiveColor = useColorModeValue('hsl(180, 12%, 5%)', 'hsl(180, 12%, 95%)')
-    return (
-        <Link
-            as={NextLink}
-            href={href}
-            scroll={false}
-            p={2}
-            bg={active ? 'hsl(180,10%,72%)' : undefined}
-            color={active ? 'hsl(180, 12%, 5%)' : inactiveColor}
-            borderRadius={5}
-            target={target}
-            {...props}
-        >
-            {children}
-        </Link>
-    )
+	const router = useRouter();
+	const active = router.pathname === href
+	const inactiveColor = useColorModeValue('hsl(180, 12%, 5%)', 'hsl(180, 12%, 95%)')
+	return (
+		<Link
+			as={NextLink}
+			href={href}
+			scroll={false}
+			p={2}
+			bg={active ? 'hsl(180,10%,72%)' : undefined}
+			color={active ? 'hsl(180, 12%, 5%)' : inactiveColor}
+			borderRadius={5}
+			target={target}
+			{...props}
+		>
+			{children}
+		</Link>
+	)
 }
 
 const MenuLink = forwardRef((props, ref) => (
-    <Link ref={ref} as={NextLink} {...props} />
+	<Link ref={ref} as={NextLink} {...props} />
 ))
 
 const Navbar = props => {
-    const { path } = props
+	const { path } = props
 
-    return (
-        <Box
-            position="fixed"
-            as="nav"
-            w="100%"
-            bg={useColorModeValue('hsla(180, 10%, 72%, 0.6)', 'hsla(330, 11%, 33%,0.6)')}
-            css={{ backdropFilter: 'blur(10px)' }}
-            zIndex={2}
-            {...props}
-        >
-            <Container
-                display="flex"
-                p={2}
-                maxW="container.md"
-                wrap="wrap"
-                align="center"
-                justify="space-between"
-            >
-                <Flex align="center" mr={5}>
-                    <Heading as="h1" size="lg" letterSpacing={'initial'}>
-                        <Logo />
-                    </Heading>
-                </Flex>
+	return (
+		<Box
+			position="fixed"
+			as="nav"
+			w="100%"
+			bg={useColorModeValue('hsla(180, 10%, 72%, 0.6)', 'hsla(330, 11%, 33%,0.6)')}
+			css={{ backdropFilter: 'blur(10px)' }}
+			zIndex={2}
+			{...props}
+		>
+			<Container
+				display="flex"
+				p={2}
+				maxW="container.md"
+				wrap="wrap"
+				align="center"
+				justify="space-between"
+			>
+				<Flex align="center" mr={5}>
+					<Heading as="h1" size="lg" letterSpacing={'initial'}>
+						<Logo />
+					</Heading>
+				</Flex>
 
-                <Stack
-                    direction={{ base: 'column', md: 'row' }}
-                    display={{ base: 'none', md: 'flex' }}
-                    width={{ base: 'full', md: 'auto' }}
-                    alignItems="center"
-                    flexGrow={1}
-                    mt={{ base: 4, md: 0 }}
-                >
-                    <LinkItem href="/projects" path={path}>
-                        <CircleOption>Projects</CircleOption>
-                    </LinkItem>
-                    <LinkItem
-                        href="https://drive.google.com/file/d/1SjE7s4Vi4iJU7mjcapinmnzeJKLyA8Ov/view?usp=sharing"
-                        path={path}
-                        target="_blank">
-                        <CircleOption>Resume</CircleOption>
-                    </LinkItem>
-                    <LinkItem
-                        target="_blank"
-                        href="https://github.com/Karidus-423/career_portfolio"
-                        path={path}
-                        display="inline-flex"
-                        alignItems="center"
-                        style={{ gap: 4 }}
-                        pl={2}
-                    >
-                        <IoLogoGithub />
-                        Source
-                    </LinkItem>
-                </Stack>
+				<Stack
+					direction={{ base: 'column', md: 'row' }}
+					display={{ base: 'none', md: 'flex' }}
+					width={{ base: 'full', md: 'auto' }}
+					alignItems="center"
+					flexGrow={1}
+					mt={{ base: 4, md: 0 }}
+				>
+					<LinkItem href="/projects" path={path}>
+						<CircleOption>Projects</CircleOption>
+					</LinkItem>
+					<LinkItem
+						href="https://drive.google.com/file/d/1oLy3Y_6nmzMHPZ4WmnEo7lGo7iVTEvrS/view?usp=sharing"
+						path={path}
+						target="_blank">
+						<CircleOption>Resume</CircleOption>
+					</LinkItem>
+					<LinkItem
+						target="_blank"
+						href="https://github.com/Karidus-423/career_portfolio"
+						path={path}
+						display="inline-flex"
+						alignItems="center"
+						style={{ gap: 4 }}
+						pl={2}
+					>
+						<IoLogoGithub />
+						Source
+					</LinkItem>
+				</Stack>
 
-                <Box flex={1} align="right">
-                    <Box mr={3} display={{ base: 'inline-block', md: 'none' }}>
-                        <Menu isLazy id='navbar-menu' backgroundColor={useColorModeValue('hsl(192, 12%, 8%)', 'hsl(192, 12%, 92%)')}>
-                            <MenuButton
-                                as={IconButton}
-                                icon={<HamburgerIcon />}
-                                variant="outline"
-                                aria-label="Options"
-                            />
-                            <MenuList >
-                                <MenuItem as={MenuLink} href="/projects">
-                                    <CircleOption> Projects </CircleOption>
-                                </MenuItem>
-                                <MenuItem as={MenuLink} href="https://drive.google.com/file/d/1SjE7s4Vi4iJU7mjcapinmnzeJKLyA8Ov/view?usp=sharing">
-                                    <CircleOption> Resume </CircleOption>
-                                </MenuItem>
-                                <MenuItem as={MenuLink} href="https://github.com/Karidus-423/career_portfolio">
-                                    Source Code
-                                </MenuItem>
-                            </MenuList>
+				<Box flex={1} align="right">
+					<Box mr={3} display={{ base: 'inline-block', md: 'none' }}>
+						<Menu isLazy id='navbar-menu' backgroundColor={useColorModeValue('hsl(192, 12%, 8%)', 'hsl(192, 12%, 92%)')}>
+							<MenuButton
+								as={IconButton}
+								icon={<HamburgerIcon />}
+								variant="outline"
+								aria-label="Options"
+							/>
+							<MenuList >
+								<MenuItem as={MenuLink} href="/projects">
+									<CircleOption> Projects </CircleOption>
+								</MenuItem>
+								<MenuItem as={MenuLink} href="https://drive.google.com/file/d/1oLy3Y_6nmzMHPZ4WmnEo7lGo7iVTEvrS/view?usp=sharing">
+									<CircleOption> Resume </CircleOption>
+								</MenuItem>
+								<MenuItem as={MenuLink} href="https://github.com/Karidus-423/career_portfolio">
+									Source Code
+								</MenuItem>
+							</MenuList>
 
-                        </Menu>
+						</Menu>
 
-                    </Box>
-                    <ThemeToggleButton />
-                </Box>
-            </Container>
-        </Box >
-    )
+					</Box>
+					<ThemeToggleButton />
+				</Box>
+			</Container>
+		</Box >
+	)
 }
 
 Navbar.displayName = 'Navber';
