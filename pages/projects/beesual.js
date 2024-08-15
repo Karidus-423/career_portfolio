91615ad32fb607ae0b76a2acec94b7e9f705c59b
Karidus-423 <kapuerto23@gmail.com>
2024-08-14 23:24:33 -0400

    fix bee page

--- pages/projects/beesual.js
+++ pages/projects/beesual.js
@@ -5,36 +5,36 @@ import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
 const Project = () => {
-    return (
-        <Layout title="Beesual">
-            <Container mt={8}>
-                <Title>
-                    Beesual <Badge>2023 ~ </Badge>
-                </Title>
-                <P>
-                    3D Beehive Visualization Tool for monitoring and analyzing of temperature
-                    patterns inside a beehive.
-                </P>
-                <List ml={4} my={4}>
-                    <ListItem>
-                        <Meta> Web Based</Meta>
-                    </ListItem>
-                    <ListItem>
-                        <Meta> Stack </Meta>
-                        <span> Plotly, Dash, Python</span>
-                    </ListItem>
-                    <ListItem>
-                        <Meta> Source Code </Meta>
-                        <Link href="https://www.github.com/Karidus-423/Bee_Hive_Webapp" target='_blank'>
-                            Github <ExternalLinkIcon mx="2px" />
-                        </Link>
-                    </ListItem>
-                </List>
-                <ProjectImage src="../../images/PCBdesign.png" alt="Beesual Site Image" />
-                <ProjectImage src="../../images/Beesual.png" alt="Beesual Site Image" />
-            </Container>
-        </Layout>
-    )
+	return (
+		<Layout title="Beesual">
+			<Container mt={8}>
+				<Title>
+					Beesual <Badge>2023 ~ </Badge>
+				</Title>
+				<P>
+					3D Beehive Visualization Tool for monitoring and analyzing of temperature
+					patterns inside a beehive.
+				</P>
+				<List ml={4} my={4}>
+					<ListItem>
+						<Meta> Web Based</Meta>
+					</ListItem>
+					<ListItem>
+						<Meta> Stack </Meta>
+						<span> Plotly, Dash, Python</span>
+					</ListItem>
+					<ListItem>
+						<Meta> Source Code </Meta>
+						<Link href="https://github.com/Karidus-423/Beehive_Project" target='_blank'>
+							Github <ExternalLinkIcon mx="2px" />
+						</Link>
+					</ListItem>
+				</List>
+				<ProjectImage src="../../images/PCBdesign.png" alt="Beesual Site Image" />
+				<ProjectImage src="../../images/Beesual.png" alt="Beesual Site Image" />
+			</Container>
+		</Layout>
+	)
 }
 
 export default Project
